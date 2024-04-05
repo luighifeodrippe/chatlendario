509903f1c6fe09d1fc3b24ea9dac3617e944e437
Luighi Feodrippe <luighi@universointeligente.com>
2024-04-05 16:07:34 -0300

    new updates

---
+++ app/login/magic-link/layout.tsx
@@ -0,0 +1,12 @@
+export default function LoginLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {/* Adicione qualquer conteúdo específico do layout de login */}
+      {children}
+    </div>
+  )
+}
