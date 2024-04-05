89eac39eb17a8b3db9ca27d4cfdb8a12b932adee
Luighi Feodrippe <luighi@universointeligente.com>
2024-04-05 16:05:03 -0300

    new updates

---
+++ app/reset-password/layout.tsx
@@ -0,0 +1,12 @@
+export default function ResetPasswordLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {/* Adicione qualquer conteúdo específico do layout de redefinição de senha */}
+      {children}
+    </div>
+  )
+}
