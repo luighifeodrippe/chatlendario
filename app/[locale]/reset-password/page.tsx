a63acccbec24f98d6adb429f4d0c9132de86639f
Luighi Feodrippe <luighi@universointeligente.com>
2024-04-05 16:25:13 -0300

    new updates

--- app/[locale]/reset-password/page.tsx
+++ app/[locale]/reset-password/page.tsx
@@ -5,16 +5,25 @@ import { SubmitButton } from "@/components/ui/submit-button"
 import { createClient } from "@/lib/supabase/server"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { useState } from "react"
 
 export default function ResetPassword({
   searchParams
 }: {
   searchParams: { message: string }
 }) {
+  const [passwordMatch, setPasswordMatch] = useState(true)
+
   const handleResetPassword = async (formData: FormData) => {
     "use server"
 
     const newPassword = formData.get("newPassword") as string
+    const confirmPassword = formData.get("confirmPassword") as string
+
+    if (newPassword !== confirmPassword) {
+      return redirect("/reset-password?message=As senhas não coincidem.")
+    }
+
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
@@ -29,7 +38,15 @@ export default function ResetPassword({
       return redirect(`/reset-password?message=${error.message}`)
     }
 
-    return redirect("/login?message=Redefinição de senha bem-sucedida")
+    return redirect("/login?message=A atualização da senha foi bem-sucedida.")
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = document.querySelector<HTMLInputElement>(
+      'input[name="newPassword"]'
+    )?.value
+    const confirmPassword = e.target.value
+    setPasswordMatch(newPassword === confirmPassword)
   }
 
   return (
@@ -41,7 +58,7 @@ export default function ResetPassword({
         <Brand />
 
         <Label className="text-md mt-4" htmlFor="newPassword">
-          Nova Senha
+          Cadastrar senha
         </Label>
         <Input
           className="mb-3 rounded-md border bg-inherit px-4 py-2"
@@ -51,8 +68,27 @@ export default function ResetPassword({
           required
         />
 
-        <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
-          Redefinir senha
+        <Label className="text-md mt-4" htmlFor="confirmPassword">
+          Confirmar senha
+        </Label>
+        <Input
+          className="mb-3 rounded-md border bg-inherit px-4 py-2"
+          type="password"
+          name="confirmPassword"
+          placeholder="••••••••"
+          onChange={handlePasswordChange}
+          required
+        />
+
+        {!passwordMatch && (
+          <p className="text-red-500">As senhas não coincidem.</p>
+        )}
+
+        <SubmitButton
+          className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white"
+          disabled={!passwordMatch}
+        >
+          Confirmar
         </SubmitButton>
 
         {searchParams?.message && (
