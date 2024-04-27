3b9618b74e53897fbc713dce9b86baad4989126d
Talles Souza <talles-swd@live.com>
2024-04-26 21:43:39 -0300

    Remoção de assistants

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -9,11 +9,12 @@ import {
   IconRobotFace,
   IconSparkles
 } from "@tabler/icons-react"
-import { FC } from "react"
+import { FC, useContext } from "react"
 import { TabsList } from "../ui/tabs"
 import { WithTooltip } from "../ui/with-tooltip"
 import { ProfileSettings } from "../utility/profile-settings"
 import { SidebarSwitchItem } from "./sidebar-switch-item"
+import { ChatbotUIContext } from "@/context/context"
 
 export const SIDEBAR_ICON_SIZE = 28
 
@@ -24,6 +25,8 @@ interface SidebarSwitcherProps {
 export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
   onContentTypeChange
 }) => {
+  const { profile, selectedWorkspace } = useContext(ChatbotUIContext)
+
   return (
     <div className="flex flex-col justify-between border-r-2 pb-5">
       <TabsList className="bg-background grid h-[440px] grid-rows-7">
@@ -33,11 +36,13 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
           onContentTypeChange={onContentTypeChange}
         />
 
-        <SidebarSwitchItem
-          icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
-          contentType="assistants"
-          onContentTypeChange={onContentTypeChange}
-        />
+        {profile?.user_id === selectedWorkspace?.user_id && (
+          <SidebarSwitchItem
+            icon={<IconRobotFace size={SIDEBAR_ICON_SIZE} />}
+            contentType="assistants"
+            onContentTypeChange={onContentTypeChange}
+          />
+        )}
 
         <SidebarSwitchItem
           icon={<IconPencil size={SIDEBAR_ICON_SIZE} />}
