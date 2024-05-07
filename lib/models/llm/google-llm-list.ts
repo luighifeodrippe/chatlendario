0f180f38e1a97b1cdd6bf7150d9e6e06741a3d8d
Talles Souza <talles-swd@live.com>
2024-05-07 16:56:12 -0300

    Correção de erro ao chamar antropic api

--- lib/models/llm/google-llm-list.ts
+++ lib/models/llm/google-llm-list.ts
@@ -24,4 +24,17 @@ const GEMINI_PRO_VISION: LLM = {
   imageInput: true
 }
 
-export const GOOGLE_LLM_LIST: LLM[] = [GEMINI_PRO, GEMINI_PRO_VISION]
+const GEMINI_PRO_15: LLM = {
+  modelId: "gemini-1.5-pro-latest",
+  modelName: "Gemini 1.5 Pro",
+  provider: "google",
+  hostedId: "gemini-1.5-pro-latest",
+  platformLink: GOOGLE_PLATORM_LINK,
+  imageInput: true
+}
+
+export const GOOGLE_LLM_LIST: LLM[] = [
+  GEMINI_PRO,
+  GEMINI_PRO_15,
+  GEMINI_PRO_VISION
+]
