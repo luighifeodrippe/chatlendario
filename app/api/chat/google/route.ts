9be862821cc51a32d94b8f1fb6fcf585de306470
Talles Souza <talles-swd@live.com>
2024-05-08 01:25:58 -0300

    Erro de cota de mensagens via api

--- app/api/chat/google/route.ts
+++ app/api/chat/google/route.ts
@@ -80,6 +80,11 @@ export async function POST(request: Request) {
     ) {
       errorMessage =
         "Para utilizar o modelo Vision insira uma imagem e o prompt que deseja que ele execute. Este modelo não suporta conversas contínuas, toda mensagem deve conter uma imagem."
+    } else if (
+      errorMessage.toLowerCase().includes("resource has been exhausted")
+    ) {
+      errorMessage =
+        "A API do google recebeu muitas solicitações, aguarde alguns segundos para enviar a mensagem."
     }
 
     return new Response(JSON.stringify({ message: errorMessage }), {
